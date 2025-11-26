84f42b7293a105519988ba2d86e9e14c80c11ada
Ramesh <rameshganig@gmail.com>
2025-11-25 20:45:51 -0500

    Fixed FastAPI DB connection and added root route

--- backend/db_helper.py
+++ backend/db_helper.py
@@ -11,7 +11,7 @@ def get_db_cursor(commit=False):
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="root",
+        password="1OX10ec425",
         database="expense_manager"
     )
 
